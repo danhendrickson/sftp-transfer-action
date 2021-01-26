85efb527eff770b39f7c94c1dea2dcdf4f114bbb
Dan Hendrickson <dh@brokenreality.com>
2021-01-26 18:10:17 -0500

    Testing

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  let Client = require('ssh2-sftp-client');
+  let sftp = new Client();
+  
+  sftp.connect({
+    host: '127.0.0.1',
+    port: '8080',
+    username: 'username',
+    password: '******'
+  }).then(() => {
+    return sftp.list('/pathname');
+  }).then(data => {
+    console.log(data, 'the data info');
+  }).catch(err => {
+    console.log(err, 'catch error');
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
